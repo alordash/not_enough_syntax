c735cca52887af1477e13fe56b28e8a81c3ff3e2
alordash <alordash1@gmail.com>
2026-09-14 19:31:53 +0400

    byte string

--- src/expr/lit.rs
+++ src/expr/lit.rs
@@ -7,3 +7,10 @@ pub fn string(span: Span, string: &str) -> Expr {
         lit: Lit::Str(LitStr::new(string, span)),
     })
 }
+
+pub fn byte_string(span: Span, byte_string: &[u8]) -> Expr {
+    Expr::Lit(ExprLit {
+        attrs: Vec::new(),
+        lit: Lit::ByteStr(LitByteStr::new(byte_string, span)),
+    })
+}
